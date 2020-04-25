486f2a61e366f3022c556fb5d820b068be131ad3
Nicolai Skye <d4rkeclipse@gmail.com>
2020-04-25 23:24:20 +0700

    Adding starter files for game (template)

---
+++ desktop/src/com/prog2game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.prog2game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.prog2game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
