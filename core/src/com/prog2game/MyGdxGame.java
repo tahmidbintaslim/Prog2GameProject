b9a1b511421886aa463ba5b4d515a0e8bb78b3fb
ThiwakonPB <50686321+ThiwakonPB@users.noreply.github.com>
2020-04-26 17:27:31 +0700

    Screen changing
    Setting up the classes and methods for screen changing.
    
    LoadingScreen – initial screen to show loading screen.
    MenuScreen – a menu screen our second screen to show.
    MainScreen – the screen which shows the game play.
    PreferencesScreen – shows the preferences and allows them to be changed.
    EndScreen – show the end game credits etc.

--- core/src/com/prog2game/MyGdxGame.java
+++ core/src/com/prog2game/MyGdxGame.java
@@ -1,37 +1,53 @@
 package com.prog2game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.actions.RunnableAction;
 
-public class MyGdxGame extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private Texture img;
+public class MyGdxGame extends Game {
+
+	private LoadingScreen loadingScreen;
+	private PreferencesScreen preferencesScreen;
+	private MenuScreen menuScreen;
+	private MainScreen mainScreen;
+	private EndScreen endScreen;
+
+	public final static int MENU = 0;
+	public final static int PREFERENCES = 1;
+	public final static int APPLICATION = 2;
+	public final static int ENDGAME = 3;
+
 
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		loadingScreen = new LoadingScreen(this);
+		setScreen(loadingScreen);
 
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0, 0, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 50, 50);
-		batch.end();
+	public void changeScreen (int screen) {
+		switch(screen){
+			case MENU:
+				if(menuScreen == null) menuScreen = new MenuScreen(this);
+				this.setScreen(menuScreen);
+				break;
+			case PREFERENCES:
+				if(preferencesScreen == null) preferencesScreen = new PreferencesScreen(this);
+				this.setScreen(preferencesScreen);
+				break;
+			case APPLICATION:
+				if(mainScreen == null) mainScreen = new MainScreen(this);
+				this.setScreen(mainScreen);
+				break;
+			case ENDGAME:
+				if(endScreen == null) endScreen = new EndScreen(this);
+				this.setScreen(endScreen);
+				break;
 
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-
+		}
 	}
 }
