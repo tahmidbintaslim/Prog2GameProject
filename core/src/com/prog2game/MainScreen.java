f2acaf00e2000e053ace1215925184a2086ae91d
Nicolai Skye <d4rkeclipse@gmail.com>
2020-04-29 18:20:03 +0700

    Updated GL20 to GL30 in MainScreen.java and MenuScreen.java

--- core/src/com/prog2game/MainScreen.java
+++ core/src/com/prog2game/MainScreen.java
@@ -2,7 +2,7 @@ package com.prog2game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -48,7 +48,7 @@ public class MainScreen implements Screen   {
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0f, 0f, 0f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         stage.draw();
     }
--- core/src/com/prog2game/MenuScreen.java
+++ core/src/com/prog2game/MenuScreen.java
@@ -2,7 +2,7 @@ package com.prog2game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
@@ -74,7 +74,7 @@ public class MenuScreen implements Screen {
 //        batch.draw(texture,100,100);
 //        batch.end();
         Gdx.gl.glClearColor(0f, 0f, 0f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         stage.getBatch().begin();
         stage.getBatch().draw(texture,150,300);
